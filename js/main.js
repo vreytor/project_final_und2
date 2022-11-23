2ab2293c5db697f9f54db0b820a7046c9141925e
vreytor <69092748+vreytor@users.noreply.github.com>
2022-11-23 02:01:12 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -5,24 +5,76 @@ En caso contyrario se muestra el emsanje de error
 */
 
 // ENTRADA
-const email = document.getElementById("txtEmail");
-const error = document.getElementById("emailVal");
-const btnEnviar = document.getElementById("btnEnviar");
-let mensaje = ""
+const email       = document.getElementById("txtEmail"),
+      nombre      = document.getElementById("txtNombre"),
+      asunto      = document.getElementById("txtAsunto"),
+      mensaje     = document.getElementById("txtMsj");
 
-btnEnviar.onclick = function (){
-    let vemail = email.value;
-    let arroba =0;
-    // PROCESOS
+const errorEmail  = document.getElementById("emailVal"),
+      errorNombre = document.getElementById("nombreVal"),
+      errorAsunto = document.getElementById("asuntoVal"),
+      errorMsj    = document.getElementById("msjVal");
 
-    arroba = vemail.indexOf("@");
-    if(arroba == -1){
-        mensaje = "Email inválido";
-    }else{
-        window.location.replace("index.html");
-        //window.location.assign("pagina1.html");
-    }
-    //SALIDA
-        error.innerText = mensaje;
+const btnEnviar   = document.getElementById("btnEnviar");
+
+let notificacion  = ""
+function callError(fieldError = '', message = 'Error') {
+  fieldError.innerText = message
+  window.alert(message)
+  return
+}
+function validarCampo( valor, type = ''){
+  let correct = true
+  switch (type) {
+    case 'email':
+      correct = valor.indexOf("@") > -1
+      break;
+    default:
+      correct = valor.trim().length > 0
+      break;
+  }
+  return correct;
 }
 
+function valorNotificacion( mensaje ){
+  return `${mensaje} no válido`;
+}
+
+btnEnviar.onclick = function (){
+    let vemail   = email.value,
+        vnombre  = nombre.value,
+        vasunto  = asunto.value,
+        vmensaje = mensaje.value;
+    let arroba   = 0;
+    // PROCESOS
+    let nombreValidado = validarCampo( vnombre );
+    if(!nombreValidado) {
+      callError(errorNombre, 'esta mal nombre')
+      return
+    } else {
+      errorNombre.innerText = ''
+    }
+    let emailValidado = validarCampo(vemail, 'email')    
+    if(!emailValidado) {
+      callError(errorEmail, 'esta mal email')
+      return
+    } else {
+      errorEmail.innerText = ''
+    }
+    let asuntoValidado = validarCampo( vasunto );
+    if(!asuntoValidado) {
+      callError(errorAsunto, 'esta mal asunto')
+      return
+    } else {
+      errorAsunto.innerText = ''
+    }
+    let mensajeValidado = validarCampo( vmensaje);
+    if(!mensajeValidado) {
+      callError(errorMsj, 'esta mal mensaje')
+      return
+    } else {
+      errorMsj.innerText = ''
+    }
+    console.log('Hello World')
+    return
+}
